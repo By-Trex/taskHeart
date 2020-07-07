63e2538f818681d2ccd5a3cc5489be5a4f22167b
Bariss Doruk <baris.doruk@gvg.com.tr>
2020-07-07 22:54:41 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import {Provider} from "react-redux"
+import { createStore , applyMiddleware } from "redux"
+import ReduxThunk from "redux-thunk"
+import reducers from "./reducers"
+
+import Router from "./router"
+
+
+
+
+export default class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+    
+    
+
+    render() {
+
+        const store = createStore(reducers,{},applyMiddleware(ReduxThunk))
+
+        return (
+            <Provider store = {store}>
+                <Router/>
+            </Provider>
+        )
+    }
+}
